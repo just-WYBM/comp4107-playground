8f96d088b7fc0717c7ce7ea1f6a8518020163936
just-WYBM <manyin201612@gmail.com>
2025-03-21 20:12:46 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/manwaiyin/SelfIntroduction.kt
@@ -0,0 +1,11 @@
+package hk.edu.hkbu.comp.comp4107.playground.manwaiyin
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is MAN Wai Yin.")
+        println("My student ID is 24202509.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
